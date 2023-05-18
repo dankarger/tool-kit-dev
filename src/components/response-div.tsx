014f7283fa543cb07a0fa75058ce4dae5bb1b9fe
dankarger <karger.dan@gmail.com>
2023-05-18 21:55:41 +0300

    working chat with db

--- src/components/response-div.tsx
+++ src/components/response-div.tsx
@@ -5,11 +5,11 @@ interface Props {
   author?: String;
 }
 
-export const ResponseDiv = ({ text, author = "assistace" }: Props) => {
+export const ResponseDiv = ({ text, author = "Assistense" }: Props) => {
   if (!text) return null;
 
   return (
-    <div className="flex h-full w-full flex-col justify-start rounded-md border-2 border-gray-300 p-2 align-middle">
+    <div className="flex h-full w-full flex-col justify-start rounded-md border-2 border-gray-300 p-4 align-middle">
       {/* <div className="w-full bg-green-100">
         <p className="leading-7 [&:not(:first-child)]:mt-6 ">{author}</p>
       </div>
--- src/server/api/routers/chat.ts
+++ src/server/api/routers/chat.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-// import openai from "@/lib/openai";
+import openai from "@/lib/openai";
 import { clerkClient } from "@clerk/nextjs";
 import {
   createTRPCRouter,
@@ -8,6 +8,15 @@ import {
 } from "@/server/api/trpc";
 import { ChatMessage } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
+
+import { Analytics, Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
 
 export const chatRouter = createTRPCRouter({
   sendPrompt: privateProcedure
@@ -23,12 +32,11 @@ export const chatRouter = createTRPCRouter({
       // });
       // console.log("response2", response.data.choices);
       // const data = response.data.choices[0].message.content;
-
-      const data = `mock response ${input}`;
-      if (!data) throw new TRPCError({ code: "NOT_FOUND" });
-      return {
-        response: `: ${data}`,
-      };
+      // const data = `mock response ${input}`;
+      // if (!data) throw new TRPCError({ code: "NOT_FOUND" });
+      // return {
+      //   response: data,
+      // };
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
@@ -41,22 +49,35 @@ export const chatRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        message: z.string().min(1).max(280),
+        message: z.string().min(1).max(480),
       })
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
-      // const { success } = await ratelimit.limit(authorId);
+      const { success } = await ratelimit.limit(authorId);
 
-      // if (!success) {
-      //   throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-      // }
-      const respone = "mock response2 ";
+      if (!success) {
+        throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+      }
+      const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: input.message }],
+        // prompt: "Say it s party time",
+        max_tokens: 90,
+        // stop: "\n",
+        temperature: 0.5,
+      });
+      console.log("response2", response.data.choices);
+      const data = response.data.choices[0].message.content;
+
+      // const data = `mock response ${input}`;
+      if (!data) throw new TRPCError({ code: "NOT_FOUND" });
+
       const chatMessage = await ctx.prisma.chatMessage.create({
         data: {
           authorId,
           message: input.message,
-          response: respone,
+          response: data,
         },
       });
       // const chat = `it will be the responce fropm openai for prompt: ${input.content}`;
